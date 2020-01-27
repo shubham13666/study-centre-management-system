e9d0ea932b3c7938bbfac80275a27091c0f2070a
shubham13666 <shubham13666@gmail.com>
2020-01-27 11:44:34 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import SignIn from "./containers/signIn";
+import SignUp from "./containers/signUp";
+import LandingPage from "./containers/landingPage";
+import Home from "./containers/Home/index";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {<Home/>}
+      {/* <SignIn /> */}
+      {/* <SignUp /> */}
+      {/* <LandingPage /> */}
     </div>
   );
 }
---
+++ src/containers/landingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "antd";
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">Login as Admin</Button>
+        <Button type="primary">Login as</Button>
+      </div>
+    );
+  }
+}
+export default LandingPage;
